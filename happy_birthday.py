4ee7ff2d54fd79b0e65dae9fdd4570c3b0b32985
ds8010 <156882507+ds8010@users.noreply.github.com>
2024-01-22 20:53:49 +0400

    Add files via upload

---
+++ happy_birthday.py
@@ -0,0 +1,5 @@
+name = input ("Enter your name:")
+month= input ("Enter your birth month:")
+day= input ("Enter birthdate of the month(eg.06):")
+year= input ("Enter birth year(eg.2022):")
+print (name+", Your birthday is on",month,day+',',year,"!")
